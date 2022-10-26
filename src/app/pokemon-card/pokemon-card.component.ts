ed9928152a823492a1b472828243564ea2ac81e4
Kadu <kadu1998@outlook.com>
2022-10-26 00:53:26 -0300

    development: Initial

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css'],
+})
+export class PokemonCardComponent {
+  @Input()
+  pokemon!: string;
+
+  @Input()
+  num!: number;
+
+  getImagePokemon() {
+    const formatedNumber = this.leadingZero(this.num);
+
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatedNumber}.png`;
+  }
+
+  leadingZero(str: string | number, size = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
+  }
+}
